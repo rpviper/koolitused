8eac205485e0a7d4d0969cd9acac381666b45726
rpviper <raino.paal@hotmail.com>
2022-03-25 11:37:13 +0200

    muutused

--- Angular-03-2022-1/Veebipood/src/app/avaleht/avaleht.component.ts
+++ Angular-03-2022-1/Veebipood/src/app/avaleht/avaleht.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';  //import node_module seest
 
-@Component({
-  selector: 'app-avaleht',
-  templateUrl: './avaleht.component.html',
-  styleUrls: ['./avaleht.component.css']
+@Component({    //tehakse komponentiks
+  selector: 'app-avaleht',  //kuidas teda kasutusele võtta
+  templateUrl: './avaleht.component.html',  //seotakse ära html
+  styleUrls: ['./avaleht.component.css']    //seotakse ära css
 })
-export class AvalehtComponent implements OnInit {
+export class AvalehtComponent implements OnInit {   //export - saaks importida
+//class - funtsioon/muutuja/class
+//AvalehtComponent - class nimetus
+//implement OnInit - ei lase ngOnit
 
-  constructor() { }
+  constructor() {console.log("pannakse avaleht construktor käima") }
 
-  ngOnInit(): void {
+  ngOnInit(): void {console.log("pannakse avaleht ngOnInit käima")
   }
 
 }
--- Angular-03-2022-1/Veebipood/src/app/ostukorv/ostukorv.component.ts
+++ Angular-03-2022-1/Veebipood/src/app/ostukorv/ostukorv.component.ts
@@ -7,9 +7,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OstukorvComponent implements OnInit {
 
-  constructor() { }
+  string = "sõnaline muutuja";
+  number = 22;      //see läheb sinna ostukorv.componet.html
+  boolean = true;   //rangelt true or false
+
+
+  constructor() {
+    console.log("pannakse ostukorv construktor käima");
+   }
 
   ngOnInit(): void {
+    console.log("pannakse ostukorv ngOnInit käima")
   }
+korrutaKahega() {
+ this.number = this.number *2;    //ja siin on seesama number kasutatud
+}
 
+muudaBoolean() {
+  this.boolean = false;
+}
+
+muudaBoolean2() {
+  this.boolean = !this.boolean;   // ! tähendab vastupidist
+}
+//funktsioon ei tööta kui ei ole () ja {}
+//funktsiooni järgi on (), tähistavad millegi vastuvõtmist - HTMLst
+//{} sulud func järel tähistavad selle func algust ja lõppu
 }
